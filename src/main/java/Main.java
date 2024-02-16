4ed24b72122e1de4888bf1ccdc73708f05d23ad7
Daniil Starov <jazzzydan@hotmail.com>
2024-02-16 19:49:58 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayList<TelevisionProgram> programs = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+
+        while(true) {
+            System.out.print("Name: ");
+            String name = scanner.nextLine();
+            if (name.isEmpty()) {
+                break;
+            }
+            System.out.print("Duration: ");
+            int duration = Integer.valueOf(scanner.nextLine());
+
+            programs.add(new TelevisionProgram(name, duration));
+
+        }
+        System.out.print("Program's maximum duration? ");
+        int maxDuration = Integer.valueOf(scanner.nextLine());
+        for (TelevisionProgram program : programs) {
+            if (program.getDuration() <= maxDuration) {
+                System.out.println(program);
+            }
+        }
+    }
+}
